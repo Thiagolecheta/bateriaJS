034180d0c8c6c43bf27895f37358b579c063d7ac
Thiagolecheta <thiago.lecheta1@gmail.com>
2022-06-01 21:41:40 -0300

    Pequeno projeto para exercitar JavaScript

---
+++ script.js
@@ -0,0 +1,50 @@
+document.body.addEventListener('keyup', (event)=>{
+
+    playSound(event.code.toLowerCase());
+
+})
+
+
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value;
+    if(song !== ''){
+        let songArray = song.split('');
+        playComposition(songArray)
+    }
+    })
+
+
+function playSound(sound) {
+    let audioEvent = document.querySelector(`#s_${sound}`)
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+
+    if(audioEvent){
+        audioEvent.currentTime = 0;
+        audioEvent.play();
+    }
+
+    if (keyElement){
+        keyElement.classList.add('active')
+
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active')
+        }, 300)
+    }
+
+
+}
+
+
+function playComposition(songArray){
+    
+    let wait = 0;
+    
+    for(let songItem of songArray){ 
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+    
+        wait += 250
+    }
+}
